b2fab49ffe8f6983e9feb2962f45b84447f47674
daniil787 <quantomcoil@gmail.com>
2024-08-22 19:50:24 +0300

    minor code changes

--- avrec.cpp
+++ avrec.cpp
@@ -34,7 +34,6 @@ int addRecordMain(int argc, char* argv[]) {
     }
 
 
-
     const char* srcFile = argv[1];
     
     // check for file presence
@@ -55,6 +54,8 @@ int addRecordMain(int argc, char* argv[]) {
     std::cout << "name: " << name << std::endl;
 
     
+
+
     // create record of base signatures(avbase)
     SAVRecord Record;
     Record.Signature.Offset = offset;
@@ -63,12 +64,20 @@ int addRecordMain(int argc, char* argv[]) {
     Record.allocName(Record.NameLen);
     strncpy_s(Record.Name, Record.NameLen + 1, name, _TRUNCATE);
 
+
+
+
+
     // open file in binary 
     std::ifstream hSrcFile(srcFile, std::ios::in | std::ios::binary);
     if (!hSrcFile.is_open()) {
         std::cout << "> Can't open source file. Stop." << std::endl;
         return 0;
     }
+
+
+
+
 
     // Reading data to calculate checksum
     std::unique_ptr<BYTE[]> Buffer(new BYTE[Record.Signature.Lenght]);
@@ -77,14 +86,21 @@ int addRecordMain(int argc, char* argv[]) {
         hSrcFile.close();
         return 0;
     }
+
     hSrcFile.seekg(Record.Signature.Offset, std::ios::beg);
     hSrcFile.read(reinterpret_cast<char*>(Buffer.get()), Record.Signature.Lenght);
     hSrcFile.close();
 
+
+
+
     // Calculating the signature hash
     getSHA256(Buffer.get(), Record.Signature.Lenght, reinterpret_cast<BYTE*>(Record.Signature.Hash));
+    
 
-    // Adding a record to the database
+
+
+    // print info
     std::cout << "Record info:" << std::endl;
     std::cout << " Name: " << Record.Name << std::endl;
     std::cout << " Offset: 0x" << std::hex << Record.Signature.Offset << " (" << std::dec << Record.Signature.Offset << ")" << std::endl;
@@ -94,6 +110,11 @@ int addRecordMain(int argc, char* argv[]) {
         Record.Signature.Hash[3], Record.Signature.Hash[4], Record.Signature.Hash[5],
         Record.Signature.Hash[6], Record.Signature.Hash[7]);
 
+
+
+
+
+    // Adding a record to the database
     CAVBFileWriter hAVBFile;
     if (!hAVBFile.open(dstFile)) {
         std::cout << "> Can't open database file. Stop." << std::endl;
